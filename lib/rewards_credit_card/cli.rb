558b6cfdc8bc142c32c9f45b214ba5568a3add05
Joma Ejercito <jomaejercito@gmail.com>
2018-02-22 06:02:08 +0000

    Made rough draft code in cli

--- lib/rewards_credit_card/cli.rb
+++ lib/rewards_credit_card/cli.rb
@@ -1,7 +1,28 @@
 class RewardsCreditCard::CLI
 
+
   def call
-    puts "Best Rewards Credit Cards"
+    list_cards
+  end
+
+
+  def list_cards
+    puts "Find the credit card that gives you the best rewards!"
+    puts "-----------------------------------------------------"
+    puts "Please enter the number that corresponds to the preferred reward(s)."
+    puts ""
+    puts <<-DOC
+    1. Best for: High Dining and Travel Rewards
+    2. Best for: 5% Bonus Categories
+    3. Best for: Easy Flat Rate Travel Rewards
+    4. Best for: Cash Back Bonus Match
+    5. Best for: Gas and Grocery Rewards
+    DOC
+  end
+
+
+  def menu
+
   end
 
 
