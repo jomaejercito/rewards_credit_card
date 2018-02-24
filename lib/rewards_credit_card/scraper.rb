1d2bbe49c699580f73b599ec6fcee9dfb8aa725c
Joma Ejercito <jomaejercito@gmail.com>
2018-02-24 02:15:23 +0000

    Edited methods in scraper.rb

--- lib/rewards_credit_card/scraper.rb
+++ lib/rewards_credit_card/scraper.rb
@@ -1,23 +1,26 @@
 class RewardsCreditCard::Scraper
 
 
-  def method_name
+  def get_page
     doc = Nokogiri::HTML(open(https://www.nerdwallet.com/the-best-credit-cards))
-
-    doc.css("section#rewards.bcc-section").each do |cards|
-      card.best_for = cards.css(<"span.card-image-apply-set__card-desc _136gj")
-      card.name = cards.css("h3.card-image-apply-set__card-name _1Fuct")
-      card.benefits = cards.css("ul.card-marketing-bullets _35sc7">)
-      card.intro_apr = cards.css(#{}"p.card-details__content _136gj")
-      card.regular_apr = cards.css(#{}"p.card-details__content _136gj")
-      card.annual_fee = cards.css(#{}"p.card-details__content _136gj")
-      card.recommended_credit_score = cards.css("span.likelihood-of-approval__label likelihood-of-approval__label--min")
-    end
   end
 
 
-  def method_name
+  def get_card
+    get_page.css("section#rewards.bcc-section")
+  end
 
+
+  def make_card
+    self.get_card.each do |x|
+      card.best_for = x.css(<"span.card-image-apply-set__card-desc _136gj").text
+      card.name = x.css("h3.card-image-apply-set__card-name _1Fuct").text
+      card.benefits = x.css("ul.card-marketing-bullets _35sc7">).text
+      card.intro_apr = x.css(#{}"p.card-details__content _136gj").text
+      card.regular_apr = x.css(#{}"p.card-details__content _136gj").text
+      card.annual_fee = x.css(#{}"p.card-details__content _136gj").text
+      card.recommended_credit_score = x.css("span.likelihood-of-approval__label likelihood-of-approval__label--min").text
+    end    
   end
 
 
