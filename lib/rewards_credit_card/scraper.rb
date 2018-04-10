6683fb4c85cf68d4682aa5feb473e33bf9b46291
Joma Ejercito <jomaejercito@gmail.com>
2018-04-10 20:07:25 +0000

    Added .chop in card.benefits to remove the extra period.

--- lib/rewards_credit_card/scraper.rb
+++ lib/rewards_credit_card/scraper.rb
@@ -7,7 +7,7 @@ class RewardsCreditCard::Scraper
       card = RewardsCreditCard::Card.new
       card.best_for = x.css("h2.nw-cc-roundup-section-heading").first.text
       card.name = x.css("h3.card-image-apply-set__card-name").first.text
-      card.benefits = x.css("div.nw-cc-roundup-section-card-editorial p").first.text
+      card.benefits = x.css("div.nw-cc-roundup-section-card-editorial p").first.text.chop
       card.intro_apr = x.css("ul.card-details li:first-child p.card-details__content").text
       card.regular_apr = x.css("ul.card-details li:nth-child(2) p.card-details__content").text
       card.annual_fee = x.css("ul.card-details li:nth-child(3) p.card-details__content").text
